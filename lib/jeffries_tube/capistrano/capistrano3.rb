7f2c152e201d374fad9930a4b18132d85dbb51de
Brian Samson <brian@briansamson.com>
2014-11-13 10:43:43 -0600

    use correct port number for cap tasks

--- lib/jeffries_tube/capistrano/capistrano3.rb
+++ lib/jeffries_tube/capistrano/capistrano3.rb
@@ -1,12 +1,12 @@
 namespace :rails do
   desc "Open the rails console on each of the remote servers"
   task :console do
-    on roles(:app), primary: true do |host, user| 
+    on roles(:app), primary: true do |host, user|
       rails_env = fetch(:stage)
       run_interactively "RAILS_ENV=#{rails_env} bundle exec rails console"
     end
   end
- 
+
   desc "Open the rails dbconsole on each of the remote servers"
   task :dbconsole do
     on roles(:db), primary: true do |host|
@@ -14,10 +14,9 @@ namespace :rails do
       run_interactively "RAILS_ENV=#{rails_env} bundle exec rails dbconsole"
     end
   end
- 
+
   def run_interactively(command)
-    port = fetch(:port) || 22
-    puts "ssh #{host.user}@#{host} -p #{port} -t 'cd #{deploy_to}/current && #{command}'"
-    exec "ssh #{host.user}@#{host} -p #{port} -t 'cd #{deploy_to}/current && #{command}'"
+    puts "ssh #{host.user}@#{host} -p #{host.port} -t 'cd #{deploy_to}/current; #{command}'"
+    exec "ssh #{host.user}@#{host} -p #{host.port} -t 'cd #{deploy_to}/current; #{command}'"
   end
 end
