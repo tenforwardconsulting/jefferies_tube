38e484e7852ab8ada415f79f5239a9c47bea48fa
Brian Samson <brian@briansamson.com>
2015-08-06 10:00:45 -0500

    update pg_dump to be compatible with the 9.1 syntax

--- lib/jefferies_tube/postgresql_backup_adapter.rb
+++ lib/jefferies_tube/postgresql_backup_adapter.rb
@@ -1,22 +1,18 @@
 class PostgresqlBackupAdapter
   def create_backup(file)
-    echo_run "#{password_option} pg_dump --verbose -Fc \
-    #{host_option} #{username_option} -d #{database} \
-    --file #{file}"
+    `#{password_option} pg_dump --verbose -Fc \
+    #{host_option} #{username_option} --file #{file} \
+    #{database}
+    `
   end
 
   def restore(file)
-    echo_run "#{password_option} pg_restore --verbose --clean --no-acl --no-owner \
+    `#{password_option} pg_restore --verbose --clean --no-acl --no-owner \
     #{host_option} #{username_option} -d #{database} \
-    #{file}"
+    #{file}`
   end
 
   private
-
-  def echo_run(cmd)
-    puts ">> #{cmd}"
-    `#{cmd}`
-  end
 
   def db_option(name)
     value = db_config(name)
--- lib/jefferies_tube/version.rb
+++ lib/jefferies_tube/version.rb
@@ -1,3 +1,3 @@
 module JefferiesTube
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 end
