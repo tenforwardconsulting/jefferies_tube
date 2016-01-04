afa56bd76e0907de44dbe2df9f71c87db0abf313
Brian Samson <brian@briansamson.com>
2016-01-04 11:25:33 -0600

    add a comment explaining this madness

--- lib/jefferies_tube/railtie.rb
+++ lib/jefferies_tube/railtie.rb
@@ -10,6 +10,8 @@ module JefferiesTube
 
     config.after_initialize do |args|
       begin
+        # if this route exists, it means the app already defined its own catchall route
+        # if not, this will raise an exception and we will install our catchall instead
         ::Rails.application.routes.recognize_path("/jefferies_tube_404_test_route_test_supertest")
       rescue ActionController::RoutingError
         ::Rails.application.routes.append do
