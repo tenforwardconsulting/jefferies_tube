f745bd887706fdd4048ed8511757d253eb29c7c9
Brian Samson <brian@briansamson.com>
2016-04-23 15:50:55 -0500

    log 404's with a referer to rollbar

--- app/controllers/jefferies_tube/errors_controller.rb
+++ app/controllers/jefferies_tube/errors_controller.rb
@@ -13,6 +13,7 @@ class JefferiesTube::ErrorsController < ApplicationController
   def render_error_page(code)
     # boolean based on if there is a default layout for the current mime type
     layout = !!self.send(:_layout)
+    log_404
     begin
       render template: "/errors/#{code}", layout: layout, status: code
     rescue ActionView::MissingTemplate
@@ -23,6 +24,12 @@ class JefferiesTube::ErrorsController < ApplicationController
   def disable_pundit
     if defined?(Pundit)
       skip_authorization
+    end
+  end
+
+  def log_404
+    if defined?(Rollbar) && request.referrer.present?
+      Rollbar.warn("Got 404 with referrer", referrer: request.referrer, current_path: request.path)
     end
   end
 end
