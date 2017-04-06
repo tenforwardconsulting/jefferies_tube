8460ee720b357d5c448f21e6cfbdbf2f67414aee
Zach Boatrite <zach@tenforwardconsulting.com>
2017-04-06 12:11:08 -0500

    Don't hardcode app name into ssh command

--- lib/jefferies_tube/capistrano/ssh.rb
+++ lib/jefferies_tube/capistrano/ssh.rb
@@ -2,7 +2,7 @@ desc "Open an ssh session"
 task :ssh do
   on roles(:app), primary: true do |host|
     port = host.port || 22
-    cmd = %Q|ssh #{host.user}@#{host} -p #{port} -t "cd /u/apps/stimmi/current && exec bash -l"|
+    cmd = %Q|ssh #{host.user}@#{host} -p #{port} -t "cd /u/apps/#{fetch :application}/current && exec bash -l"|
     puts cmd
     exec cmd
   end
