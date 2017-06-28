2dccd5720f28e5d8646732fe0ded2ee375af0bd7
Brian Samson <brian@briansamson.com>
2017-06-28 11:40:50 -0500

    fix this thing

--- lib/jefferies_tube/version.rb
+++ lib/jefferies_tube/version.rb
@@ -2,7 +2,7 @@ module JefferiesTube
   VERSION = "0.1.3"
 
   def self.latest_rubygems_version
-    JSON.parse(Net::HTTP.get('rubygems.org', '/api/v1/versions/jefferies_tube/latest.json'))["version"]
+    JSON.parse(URI.parse("https://rubygems.org/api/v1/versions/jefferies_tube/latest.json").read)["version"]
   rescue
     nil
   end
