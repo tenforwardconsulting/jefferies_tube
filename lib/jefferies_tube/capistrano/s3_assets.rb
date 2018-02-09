7535ebc37243f76d333bc51385433f5721c47a7e
Zach Boatrite <zach@tenforwardconsulting.com>
2018-02-09 14:15:29 -0600

    Don't precompile assets
    This task won't precompile assets. The way this is being used is now
    different. It will instead be configured in the deploy files to run
    after Capistrano's built in asset precompilation.
    
    The entire thing will work magically by just setting assets_roles to
    whatever arbitrary thing like "assets_precompiler" and having that role
    on only a single server

--- lib/jefferies_tube/capistrano/s3_assets.rb
+++ lib/jefferies_tube/capistrano/s3_assets.rb
@@ -1,10 +1,6 @@
 task :upload_assets_to_s3 do
   on roles(fetch(:assets_roles)) do
     within release_path do
-      with rails_env: fetch(:rails_env), rails_groups: fetch(:rails_assets_groups) do
-        execute :rake, "assets:precompile"
-      end
-
       appyml_text = capture :cat, "#{deploy_to}/shared/config/application.yml"
       appyml = YAML.load appyml_text
       execute "AWS_ACCESS_KEY_ID=#{appyml['AWS_ACCESS_KEY_ID']}", "AWS_SECRET_ACCESS_KEY=#{appyml["AWS_SECRET_ACCESS_KEY"]}", 'aws', 's3', 'sync', 'public/assets/', 's3://mobile-doorman-dev/assets'
