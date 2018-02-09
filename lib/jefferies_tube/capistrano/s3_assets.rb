7bcfc5155abc635e7c4e0b31c7d28ba2beaccb26
Zach Boatrite <zach@tenforwardconsulting.com>
2018-02-09 14:16:36 -0600

    Don't hardcode s3 destiation

--- lib/jefferies_tube/capistrano/s3_assets.rb
+++ lib/jefferies_tube/capistrano/s3_assets.rb
@@ -3,7 +3,7 @@ task :upload_assets_to_s3 do
     within release_path do
       appyml_text = capture :cat, "#{deploy_to}/shared/config/application.yml"
       appyml = YAML.load appyml_text
-      execute "AWS_ACCESS_KEY_ID=#{appyml['AWS_ACCESS_KEY_ID']}", "AWS_SECRET_ACCESS_KEY=#{appyml["AWS_SECRET_ACCESS_KEY"]}", 'aws', 's3', 'sync', 'public/assets/', 's3://mobile-doorman-dev/assets'
+      execute "AWS_ACCESS_KEY_ID=#{appyml['AWS_ACCESS_KEY_ID']}", "AWS_SECRET_ACCESS_KEY=#{appyml["AWS_SECRET_ACCESS_KEY"]}", 'aws', 's3', 'sync', 'public/assets/', fetch(:s3_destination)
     end
   end
 end
