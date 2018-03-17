fc8f20ee68a8163e4c4bfd09dfe97868cc555a05
Brian Samson <brian@briansamson.com>
2018-03-17 12:21:35 -0500

    sync sprockets manifest after uploading to s3

--- lib/jefferies_tube/capistrano/s3_assets.rb
+++ lib/jefferies_tube/capistrano/s3_assets.rb
@@ -7,3 +7,21 @@ task :upload_assets_to_s3 do
     end
   end
 end
+
+# From https://github.com/capistrano/rails/issues/89
+task :copy_assets_manifest do
+  next unless roles(:web, :app).count > 1
+  manifest_contents, manifest_name = nil, nil
+  assets_path = release_path.join('public', fetch(:assets_prefix))
+
+  on roles(fetch(:assets_roles)), primary: true do
+    manifest_name = capture(:ls, assets_path.join('.sprockets-manifest*')).strip
+    manifest_contents = download! assets_path.join(manifest_name)
+  end
+  on roles(:app) do
+    execute :rm, '-f', assets_path.join('.sprockets-manifest*')
+    upload! StringIO.new(manifest_contents), assets_path.join(manifest_name)
+  end
+end
+
+after "upload_assets_to_s3", :copy_assets_manifest
