575cda57af599e251784e32161c836bb7f08a98c
Zach Boatrite <zach@tenforwardconsulting.com>
2018-05-29 15:15:46 -0500

    Bump version to 0.1.8

--- lib/jefferies_tube/version.rb
+++ lib/jefferies_tube/version.rb
@@ -1,5 +1,5 @@
 module JefferiesTube
-  VERSION = "0.1.7"
+  VERSION = "0.1.8"
 
   def self.latest_rubygems_version
     JSON.parse(URI.parse("https://rubygems.org/api/v1/versions/jefferies_tube/latest.json").read)["version"]
