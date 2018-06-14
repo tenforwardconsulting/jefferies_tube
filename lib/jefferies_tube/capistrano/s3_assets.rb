59ec99858307f8a7e38f82f41fd7f001a3ffea96
Zach Boatrite <zach@tenforwardconsulting.com>
2018-06-14 17:21:29 -0500

    Support uploading webpack packs directory to s3

--- lib/jefferies_tube/capistrano/s3_assets.rb
+++ lib/jefferies_tube/capistrano/s3_assets.rb
@@ -3,7 +3,10 @@ task :upload_assets_to_s3 do
     within release_path do
       appyml_text = capture :cat, "#{deploy_to}/shared/config/application.yml"
       appyml = YAML.load appyml_text
-      execute "AWS_ACCESS_KEY_ID=#{appyml['AWS_ACCESS_KEY_ID']}", "AWS_SECRET_ACCESS_KEY=#{appyml["AWS_SECRET_ACCESS_KEY"]}", 'aws', 's3', 'sync', 'public/assets/', fetch(:s3_destination)
+      execute "AWS_ACCESS_KEY_ID=#{appyml['AWS_ACCESS_KEY_ID']}", "AWS_SECRET_ACCESS_KEY=#{appyml["AWS_SECRET_ACCESS_KEY"]}", 'aws', 's3', 'sync', 'public/assets/', "#{fetch(:s3_destination)}/assets"
+      if Dir.exists?('public/packs')
+        execute "AWS_ACCESS_KEY_ID=#{appyml['AWS_ACCESS_KEY_ID']}", "AWS_SECRET_ACCESS_KEY=#{appyml["AWS_SECRET_ACCESS_KEY"]}", 'aws', 's3', 'sync', 'public/packs/', "#{fetch(:s3_destination)}/packs"
+      end
     end
   end
 end
