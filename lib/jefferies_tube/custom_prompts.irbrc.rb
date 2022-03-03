e3abd9fb74450e0fac6eed952f5945c033cd4bde
Brian Samson <brian@briansamson.com>
2022-03-03 11:48:16 -0600

    update jt console overrides

--- lib/jefferies_tube/custom_prompts.irbrc.rb
+++ lib/jefferies_tube/custom_prompts.irbrc.rb
@@ -1,11 +1,15 @@
-prompt = JefferiesTube::Console.prompt
+prompt = JefferiesTube::Console.prompt_base
 
-IRB.conf[:PROMPT][:RAILS_ENV] = {
-    :PROMPT_I => prompt,
-    :PROMPT_N => prompt,
-    :PROMPT_S => nil,
-    :PROMPT_C => prompt.sub('> ', '>* '),
-    :RETURN => "=> %s\n"
+IRB.conf[:PROMPT][:JEFFERIES_TUBE] = {
+  :PROMPT_I => "#{prompt} > ",
+  :PROMPT_N => "#{prompt}%l> ",
+  :PROMPT_S => "#{prompt}%l> ",
+  :PROMPT_C => "#{prompt}*> ",
+  :RETURN => "  => %s\n"
 }
 
-IRB.conf[:PROMPT_MODE] = :RAILS_ENV
+if ENV["JEFFERIES_TUBE_IRB"]
+  IRB.conf[:USE_MULTILINE] = false
+  IRB.conf[:USE_AUTOCOMPLETE] = false
+  IRB.conf[:PROMPT_MODE] = :JEFFERIES_TUBE
+end
