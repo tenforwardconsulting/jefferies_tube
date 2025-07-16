af543f23db3abab348492e7f4df91bd34a65b8ea
Ashlie Crooks <ashlie@tenforwardconsulting.com>
2025-07-16 17:40:10 -0500

    fix: change 'exists' to 'exist'

--- lib/jefferies_tube/capistrano/s3_assets.rb
+++ lib/jefferies_tube/capistrano/s3_assets.rb
@@ -4,10 +4,10 @@ task :upload_assets_to_s3 do
       appyml_text = capture :cat, "#{deploy_to}/shared/config/application.yml"
       appyml = YAML.load appyml_text
       execute "AWS_ACCESS_KEY_ID=#{appyml['AWS_ACCESS_KEY_ID']}", "AWS_SECRET_ACCESS_KEY=#{appyml["AWS_SECRET_ACCESS_KEY"]}", 'aws', 's3', 'sync', 'public/assets/', "#{fetch(:s3_destination)}/assets"
-      if Dir.exists?('public/static_assets')
+      if Dir.exist?('public/static_assets')
         execute "AWS_ACCESS_KEY_ID=#{appyml['AWS_ACCESS_KEY_ID']}", "AWS_SECRET_ACCESS_KEY=#{appyml["AWS_SECRET_ACCESS_KEY"]}", 'aws', 's3', 'sync', 'public/static_assets/', "#{fetch(:s3_destination)}/static_assets"
       end
-      if Dir.exists?('public/packs')
+      if Dir.exist?('public/packs')
         execute "AWS_ACCESS_KEY_ID=#{appyml['AWS_ACCESS_KEY_ID']}", "AWS_SECRET_ACCESS_KEY=#{appyml["AWS_SECRET_ACCESS_KEY"]}", 'aws', 's3', 'sync', 'public/packs/', "#{fetch(:s3_destination)}/packs"
       end
     end
@@ -28,7 +28,7 @@ task :copy_assets_manifest do
     manifest_name = capture(:ls, assets_path.join('.sprockets-manifest*')).strip
     manifest_contents = download! assets_path.join(manifest_name)
     within release_path do
-      webpack_enabled = Dir.exists?('public/packs')
+      webpack_enabled = Dir.exist?('public/packs')
     end
     if webpack_enabled
       packs_manifest_name = capture(:ls, packs_path.join('manifest.json')).strip
